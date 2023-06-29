508a714daab4fa71a316ede247c99b15d2c3cf19
Mogamat-Muneeb <davimuneeb785@gmail.com>
2023-06-29 20:00:55 +0200

    wip

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+  const goalInputHandle = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+  const addGoalHandle = () => {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandle}
+        />
+        <Button title="Add Goal" onPress={addGoalHandle} />
+      </View>
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((goal) => (
+          <Text style={styles.textInput2} key={goal.id}>{goal}</Text>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: "5%",
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: 24,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "purple",
+    width: "80%",
+    padding: 8,
+    marginRight: 3,
+  },
+  goalsContainer: {
+    flex: 3,
+  },
+  textInput2: {
+    color: "red",
+    paddingVertical: 4
   },
 });
